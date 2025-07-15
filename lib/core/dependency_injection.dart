63e4f84684aacbff3b0548c4377ba7cd11a5776d
Mohamed Ali <alymohamad91@gmail.com>
2025-07-16 01:48:50 +0300

    arrange home data

--- lib/core/dependency_injection.dart
+++ lib/core/dependency_injection.dart
@@ -4,6 +4,11 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get_it/get_it.dart';
 import 'package:movie_assignment_axis/core/networking/interceptors/headers_interceptor.dart';
 import 'package:movie_assignment_axis/core/networking/interceptors/response_validation_interceptor.dart';
+import 'package:movie_assignment_axis/features/home/data/datasource/home_data_source.dart';
+import 'package:movie_assignment_axis/features/home/data/repo/home_repo_impl.dart';
+import 'package:movie_assignment_axis/features/home/domain/repo/home_repo.dart';
+import 'package:movie_assignment_axis/features/home/domain/usecase/get_populars_use_case.dart';
+import 'package:movie_assignment_axis/features/home/presentation/cubit/home_cubit.dart';
 import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
 final sl = GetIt.instance;
@@ -21,6 +26,11 @@ setUp() async {
         )
       ]),
   );
+
+  sl.registerFactory<HomeDataSource>(() => HomeDataSourceImpl(sl()));
+  sl.registerFactory<HomeRepo>(() => HomeRepoImpl(sl()));
+  sl.registerFactory(() => GetPopularsUseCase(sl()));
+  sl.registerLazySingleton(() => HomeCubit(sl()));
 
   // sl.registerFactory(() => OtpListenerService());
   // sl.registerLazySingleton(() => UrlLauncherService());
